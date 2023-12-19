d529912fcc082ee3d66fccff85958109fc9bab45
Glory ann Conwi <gaconwi@googlemail.com>
2023-12-19 21:31:24 +0100

    Challenge: Backend api routes random character

---
+++ pages/api/random-character.js
@@ -0,0 +1,16 @@
+import Chance from "chance";
+
+const chance = new Chance();
+export default function handler(request, response) {
+    const character = {
+        name: chance.name(),
+        age: chance.age(),
+        birthday: chance.birthday(),
+        twitter: chance.twitter(),
+        geohash: chance.geohash()
+    };
+
+    response.status(200).json(character);
+    return;
+
+}
--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,24 @@
+import useSWR from "swr";
+
+const fetcher = (...args) => fetch(...args).then((res) => res.json());
+
 export default function HomePage() {
-  return <h1>Hello from Next.js!</h1>;
+  const { data, isLoading, error } = useSWR("/api/random-character", fetcher);
+  if (isLoading) {
+    return <h1>Loading...</h1>;
+  }
+  if (error) {
+    return <h1>Error</h1>;
+  }
+  return (
+    <div>
+      <h2>{data.name}</h2>
+      <p>
+        <strong>{data.twitter}</strong>
+      </p>
+      <p>
+        <strong>{data.geohash}</strong>
+      </p>
+    </div>
+  );
 }
