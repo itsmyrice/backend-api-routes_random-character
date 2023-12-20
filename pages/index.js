a3ee249a7039a277e307d9a9b3bf3c4613ecb2a7
Glory ann Conwi <gaconwi@googlemail.com>
2023-12-20 12:54:29 +0100

    added random button with mutate()

--- pages/index.js
+++ pages/index.js
@@ -3,22 +3,32 @@ import useSWR from "swr";
 const fetcher = (...args) => fetch(...args).then((res) => res.json());
 
 export default function HomePage() {
-  const { data, isLoading, error } = useSWR("/api/random-character", fetcher);
+  const { data, isLoading, error, mutate } = useSWR("/api/random-character", fetcher);
   if (isLoading) {
-    return <h1>Loading...</h1>;
+    return <h2>Loading...</h2>;
   }
   if (error) {
-    return <h1>Error</h1>;
+    return <h2>Error</h2>;
   }
   return (
     <div>
-      <h2>{data.name}</h2>
+      <h1>Random Character</h1>
+      <h3>Name: {data.name}</h3>
       <p>
-        <strong>{data.twitter}</strong>
+        <strong>Age: {data.age}</strong>
       </p>
       <p>
-        <strong>{data.geohash}</strong>
+        <strong>Birthday: {data.birthday}</strong>
       </p>
+      <p>
+        <strong>Twitter: {data.twitter}</strong>
+      </p>
+      <p>
+        <strong>Geohash: {data.geohash}</strong>
+      </p>
+      <div>
+        <button onClick={() => mutate()}>random</button>
+      </div>
     </div>
   );
 }
